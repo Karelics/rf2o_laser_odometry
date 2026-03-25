577f22116fb10420be0c6242bd832c357a4b06e6
Pekka Myller <pekka.myller@karelics.fi>
2026-03-25 19:23:19 +0200

    WIP

--- src/CLaserOdometry2DNode.cpp
+++ src/CLaserOdometry2DNode.cpp
@@ -137,8 +137,8 @@ void CLaserOdometry2DNode::publish()
   tf_quaternion.setRPY(0.0, 0.0, rf2o::getYaw(rf2o_ref.robot_pose_.rotation()));
   geometry_msgs::msg::Quaternion quaternion = tf2::toMsg(tf_quaternion);
   nav_msgs::msg::Odometry odom;
-
-  odom.header.stamp = rf2o_ref.last_odom_time;
+  rclcpp::Time stamp = get_clock()->now();
+  odom.header.stamp = stamp;
   odom.header.frame_id = odom_frame_id;
   //set the position
   odom.pose.pose.position.x = rf2o_ref.robot_pose_.translation()(0);
@@ -159,7 +159,7 @@ void CLaserOdometry2DNode::publish()
   {
     RCLCPP_DEBUG(get_logger(), "[rf2o] Publishing TF: [base_link] to [odom]");
     geometry_msgs::msg::TransformStamped odom_trans;
-    odom_trans.header.stamp = rf2o_ref.last_odom_time;
+    odom_trans.header.stamp = stamp;
     odom_trans.header.frame_id = odom_frame_id;
     odom_trans.child_frame_id = base_frame_id;
     odom_trans.transform.translation.x = rf2o_ref.robot_pose_.translation()(0);
